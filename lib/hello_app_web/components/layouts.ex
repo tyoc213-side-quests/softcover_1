20b4cfe79eb58680a95c9bec77c5b9035ad69b69
David <506234+tyoc213@users.noreply.github.com>
2024-04-22 21:41:11 -0600

    feat: init

---
+++ lib/hello_app_web/components/layouts.ex
@@ -0,0 +1,14 @@
+defmodule HelloAppWeb.Layouts do
+  @moduledoc """
+  This module holds different layouts used by your application.
+
+  See the `layouts` directory for all templates available.
+  The "root" layout is a skeleton rendered as part of the
+  application router. The "app" layout is set as the default
+  layout on both `use HelloAppWeb, :controller` and
+  `use HelloAppWeb, :live_view`.
+  """
+  use HelloAppWeb, :html
+
+  embed_templates "layouts/*"
+end
